027407723beb15d957a3aed8726611f476d9931d
orjon <github@orjon.com>
2019-06-04 20:53:49 +0100

    navRebuild

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,30 @@ $(() => {
 
   const animationSpeed= 3600
 
+  const currentPage = document.querySelector('.navText').innerHTML
+  const navIcons = document.querySelectorAll('.navIcon')
+  const navText = document.querySelector('.navText')
+
+  navIcons.forEach(icon => {
+    icon.addEventListener('mouseover', (e) => {
+      navText.innerHTML=''
+      if ($(e.target).hasClass('code')) {
+        navText.innerHTML='code'
+      } else if ($(e.target).hasClass('design')) {
+        navText.innerHTML='design & visualisation'
+      } else if ($(e.target).hasClass('electronics')) {
+        navText.innerHTML='electronics'
+      } else if ($(e.target).hasClass('face')) {
+        navText.innerHTML='about me'
+      }
+    })
+
+    icon.addEventListener('mouseout', () => {
+      navText.innerHTML= currentPage
+    })
+  })
+
+
   $('.colorFade > div:gt(0)').hide()
   $('.timeFade > div:gt(0)').hide()
 
